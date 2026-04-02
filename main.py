6f5fac5ef41a850e62925340256225c7e8e39c51
MichalPolak01 <michalpl618@gmail.com>
2026-04-02 18:31:44 +0200

    Implement login functionality.

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from my-library!")
+def logIn(counter: int = 0) -> bool:
 
+    login=input("Podaj login: ")
+    hasło=input("Podaj hasło: ")
+
+    for user in users:
+        if user["login"]==login and user["hasło"]==hasło:
+            print("Zalogowano pomyślnie.")
+            return True
+    else:
+        counter+=1
+
+    if counter==3:
+        print("Zbyt wiele prób logowania. Konto zablokowane.")
+        return False
+    
+    print("Nieprawidłowy login lub hasło. Spróbuj ponownie. Pozostało prób:", 3-counter)
+    return logIn(counter)
+
+
+
+users=[{"login": "admin", "hasło": "password"},{"login": "user", "hasło": "1234"},{"login": "michal", "hasło": "qwe123"}]
 
 if __name__ == "__main__":
-    main()
+    logIn()
