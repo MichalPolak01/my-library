9da59a4cb06969420553910ba4fbea3f30aab1f3
MichalPolak01 <michalpl618@gmail.com>
2026-04-02 20:15:54 +0200

    Implement borrow book functionality.

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def logIn(counter: int = 0) -> bool:
     for user in users:
         if user["login"]==login and user["hasło"]==hasło:
             print("Zalogowano pomyślnie.")
+            currentUser = user["id"]
             return True
     else:
         counter+=1
@@ -32,11 +33,10 @@ def mainMenu():
 
     match option:
         case "1":
-            print("Przeglądanie katalogu...")
             browseCatalog()
             mainMenu()
         case "2":
-            print("Wypożyczanie książki...")
+            borrowBook()
             mainMenu()
         case "3":
             print("Moje wypożyczenia...")
@@ -52,20 +52,40 @@ def browseCatalog():
     for book in books:
         print(f"{book['name']} - {book['author']} (Dostępne: {book['available']})")
 
+def borrowBook():
+    print("\n~ Wypożyczanie książki ~\n")
+    bookName=input("Podaj nazwę książki: ")
+
+    for book in books:
+        if bookName in book["name"]:
+            if any(loan["user"] == currentUser and loan["book"] == book["id"] for loan in loans):
+                print("Już wypożyczyłeś tę książkę.")
+            elif book["available"]>0:
+                book["available"]-=1
+                loans.append({"user": currentUser, "book": book["id"]})
+                print("Wypożyczenie książki przebiegło pomyślnie.")
+            else:
+                print("Niestety, ta książka jest obecnie niedostępna.")
+
 users=[
-    {"login": "michal", "hasło": "qwe123", "role": "czytelnik"},
-    {"login": "adam35", "hasło": "password", "role": "czytelnik"},
-    {"login": "krzys123", "hasło": "1234", "role": "czytelnik"}
+    {"id": 1, "login": "michal", "hasło": "qwe123", "role": "czytelnik"},
+    {"id": 2, "login": "adam35", "hasło": "password", "role": "czytelnik"},
+    {"id": 3, "login": "krzys123", "hasło": "1234", "role": "czytelnik"}
 ]
 
 books=[
-    {"name": "W pustyni i w puszczy", "author": "Henryk Sienkiewicz", "available": 5},
-    {"name": "Lalka", "author": "Bolesław Prus", "available": 3},
-    {"name": "Pan Tadeusz", "author": "Adam Mickiewicz", "available": 2},
-    {"name": "Zbrodnia i kara", "author": "Fiodor Dostojewski", "available": 4},
-    {"name": "Harry Potter i Kamień Filozoficzny", "author": "J.K. Rowling", "available": 6}
+    {"id": 1, "name": "W pustyni i w puszczy", "author": "Henryk Sienkiewicz", "available": 5},
+    {"id": 2, "name": "Lalka", "author": "Bolesław Prus", "available": 0},
+    {"id": 3, "name": "Pan Tadeusz", "author": "Adam Mickiewicz", "available": 2},
+    {"id": 4, "name": "Zbrodnia i kara", "author": "Fiodor Dostojewski", "available": 4},
+    {"id": 5, "name": "Harry Potter i Kamień Filozoficzny", "author": "J.K. Rowling", "available": 6}
 ]
 
+currentUser=None
+
+loans=[]
+
+# borrowBook()
 
 if __name__ == "__main__":
     main()
